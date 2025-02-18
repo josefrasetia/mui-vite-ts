5108f4368e62f9830a487987c3292989a0d6745d
josefrasetia <jose.frasetia@gmail.com>
2025-02-18 16:44:15 +0700

    add footer

--- src/components/layouts/DashboardLayout.tsx
+++ src/components/layouts/DashboardLayout.tsx
@@ -1,12 +1,10 @@
 import React, { useEffect, useState } from "react";
-import {
-  Box,
-  useTheme,
-  useMediaQuery,
-} from "@mui/material";
+import { Box, useTheme, useMediaQuery } from "@mui/material";
 import { Sidebar, AppBarContent } from "@components";
 
-const DashboardLayout: React.FC<{children: React.ReactNode}> = ({ children }) => {
+const DashboardLayout: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
   const [open, setOpen] = useState(!isMobile);
@@ -28,7 +26,8 @@ const DashboardLayout: React.FC<{children: React.ReactNode}> = ({ children }) =>
         open={open}
         isMobile={isMobile}
         drawerWidth={drawerWidth}
-        toggleDrawer={handleDrawerToggle} />
+        toggleDrawer={handleDrawerToggle}
+      />
 
       {/* Sidebar */}
       <Sidebar
@@ -44,16 +43,41 @@ const DashboardLayout: React.FC<{children: React.ReactNode}> = ({ children }) =>
         sx={{
           backgroundColor: theme.customColors.background.body,
           flexGrow: 1,
-          height: "100vh",
+          minHeight: "100vh",
           pt: { xs: 8.5, sm: 9.5 },
-          pl: (!isMobile && open) ? `${drawerWidth}px` : (!isMobile) ? '55px' : 0,
+          pl: !isMobile && open ? `${drawerWidth}px` : !isMobile ? "55px" : 0,
           transition: theme.transitions.create("padding", {
             easing: theme.transitions.easing.sharp,
             duration: theme.transitions.duration.enteringScreen,
           }),
         }}
       >
-        <Box maxWidth="xl" sx={{ px: 1.5 }}>{children}</Box>
+        <Box maxWidth="xl" sx={{ px: 1.5, minHeight: "90vh" }}>
+          {children}
+        </Box>
+
+        {/* Footer */}
+        <Box
+          sx={{
+            backgroundColor: "#fff",
+            boxShadow: "0 2px 20px -5px rgba(0, 0, 0, 0.1)",
+            width: "100%",
+            py: 2,
+            mt: 2,
+          }}
+        >
+          <Box maxWidth="xl" sx={{ px: 1.5 }}>
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <small>&copy; 2025 Jose Tech | All Rights Reserved</small>
+            </Box>
+          </Box>
+        </Box>
       </Box>
     </Box>
   );
